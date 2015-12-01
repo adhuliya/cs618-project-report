17c3cf5f3a3cb66324fd0dffcde6bf4a946f8430
lazy nintel <lazynintel@gmail.com>
2015-12-01 16:59:23 +0530

    adding constraints one-by-one

--- resources/test-01.c
+++ resources/test-01.c
@@ -7,23 +7,14 @@ int main() {
     int *pi;
 
     pi = &i;  
-
     i = *pi; 
-
     *pi = 19;
-
     pi = pi + 1;
-
     pi = pi - 1;
-
     pi++;      
-
     pi--;     
-
     pi = 0;   
-
     pi = 17; 
-
     func (pi);
 
     return 0;
